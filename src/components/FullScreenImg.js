d77fc6bdc6dbc5093baba2f04f95e08fee53415a
abhimanyu <iamabhimanyu555@gmail.com>
2023-11-10 16:27:36 +0530

    random

---
+++ src/components/FullScreenImg.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function FullScreenImg() {
+  return (
+    <>
+    
+    {/* <!-- Container for demo purpose --> */}
+<div class="container my-24 mx-auto md:px-6">
+  {/* <!-- Section: Design Block --> */}
+  <section class="mb-32">
+    <img src="https://mdbcdn.b-cdn.net/img/new/slides/198.jpg"
+      class="mb-6 w-full rounded-lg shadow-lg dark:shadow-black/20" alt="image" />
+
+  </section>
+  {/* <!-- Section: Design Block --> */}
+</div>
+{/* <!-- Container for demo purpose --> */}
+    
+    </>
+  )
+}
